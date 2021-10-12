ae878d8af6c026cbb9915d377062f91bd31211c5
Jack LeMasters <jacksonlemasters@outlook.com>
2021-10-12 11:47:48 -0600

    added mutations and queries

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,71 @@
+import gql from 'graphql-tag';
+
+export const LOGIN_USER = gql`
+mutation loginUser($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token
+    user {
+      _id
+    }
+  }
+}
+`;
+
+export const ADD_USER = gql`
+mutation addUser($username: String!, $password: String!, $email: String!) {
+    addUser(username: $username, password: $password, email: $email) {
+        user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            _id
+            authors
+            bookId
+            image
+            link
+            title
+            description
+        }
+        }
+        token
+    }
+}
+`;
+
+export const SAVE_BOOK = gql`
+mutation saveBook($input: SavedBookInput) {
+  saveBook(input: $input) {
+    username
+    _id
+    bookCount
+    savedBooks {
+      bookId
+      authors
+      image
+      link
+      title
+      description
+    }
+  }
+}
+`;
+
+export const REMOVE_BOOK = gql`
+mutation removeBook($bookId: String!) {
+  removeBook(bookId: $bookId) {
+    _id
+    username
+    bookCount
+    savedBooks {
+      bookId
+      authors
+      image
+      link
+      title
+      description
+    }
+  }
+}
+`;
